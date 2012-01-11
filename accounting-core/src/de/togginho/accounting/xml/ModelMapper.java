99cfa68b58fc403a038f9bbb682d342f976b4692
thorsten <thorsten@dell002>
2012-01-11 15:31:22 +0100

    code cleanup

--- accounting-core/src/de/togginho/accounting/xml/ModelMapper.java
+++ accounting-core/src/de/togginho/accounting/xml/ModelMapper.java
@@ -19,7 +19,6 @@ import java.io.File;
 import java.util.Set;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
