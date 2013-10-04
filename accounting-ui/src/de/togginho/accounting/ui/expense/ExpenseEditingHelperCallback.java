f521014a6f2f2ba270d657f9b8c3794406a6e5aa
thorsten <thorsten@dell001>
2013-10-04 19:10:34 +0200

    javadoc

--- accounting-ui/src/de/togginho/accounting/ui/expense/ExpenseEditingHelperCallback.java
+++ accounting-ui/src/de/togginho/accounting/ui/expense/ExpenseEditingHelperCallback.java
@@ -19,30 +19,46 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
+import de.togginho.accounting.model.Expense;
+
 /**
+ * A callback interface for all clients using the {@link ExpenseEditingHelper}.
+ * 
+ * <p>
+ * This interface provides for necessary customization of UI widgets as well as for custom handling in case of changes
+ * to the model by the client implementation.</p>
+ * 
  * @author thorsten
  *
  */
 interface ExpenseEditingHelperCallback {
 
 	/**
-	 * 
+	 * Called whenever the model data has changed - it is up to the implementation how to deal with these changes.
+	 * This method is only called if actual changes have been made, i.e. the currently edited {@link Expense} is 
+	 * actually in a different state than before, so clients do not have to check whether the entity is "dirty" (i.e.
+	 * in need of saving) or not.
 	 */
 	void modelHasChanged();
 	
 	/**
+	 * Called when a label with the supplied text needs to be created. Implementations are expected to supply a 
+	 * {@link Label} that is "ready to go", i.e. no further action is taken by the caller to change either the look,
+	 * layout or behavior of the label.
 	 * 
-	 * @param parent
-	 * @param text
-	 * @return
+	 * @param  parent	the parent composite for the label to be created
+	 * @param  text		the label text, should be used as-is, without any further i18n
+	 * @return the created label
 	 */
 	Label createLabel(Composite parent, String text);
 	
 	/**
+	 * Called when a {@link Text} element needs to be created. The {@link ExpenseEditingHelper} may or may not apply
+	 * further layout changes.
 	 * 
-	 * @param parent
-	 * @param style
-	 * @return
+	 * @param  parent	the parent composite to be used
+	 * @param  style		the style bits requested
+	 * @return the {@link Text} element
 	 */
 	Text createText(Composite parent, int style);
 }
