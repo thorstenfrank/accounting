4bb3081decf278aa502b169f0ea86b380a3faa82
thorsten@LE <tf@tfsw.de>
2014-09-24 12:52:07 +0200

    Fixed missing @XmlRootElement Annotation

--- accounting-core/src/de/togginho/accounting/io/xml/XmlUser.java
+++ accounting-core/src/de/togginho/accounting/io/xml/XmlUser.java
@@ -11,6 +11,7 @@ package de.togginho.accounting.io.xml;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -51,6 +52,7 @@ import javax.xml.bind.annotation.XmlType;
     "invoices",
     "expenses"
 })
+@XmlRootElement
 public class XmlUser {
 
     protected String description;
