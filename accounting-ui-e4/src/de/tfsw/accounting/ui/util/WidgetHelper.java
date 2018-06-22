2a19c95939c8b261ee89efccb39f210b9eaf35e0
Thorsten Frank <thorsten@tfsw.de>
2018-06-22 18:38:41 +0200

    added UI helper

---
+++ accounting-ui-e4/src/de/tfsw/accounting/ui/util/WidgetHelper.java
@@ -0,0 +1,74 @@
+/*
+ *  Copyright 2018 Thorsten Frank (accounting@tfsw.de).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package de.tfsw.accounting.ui.util;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+/**
+ * UI helper methods.
+ * 
+ * @author thorsten
+ */
+public final class WidgetHelper {
+
+	private static final GridDataFactory GRAB_HORIZONTAL = GridDataFactory.fillDefaults().grab(true, false);
+	
+	/**
+	 * Creates a new label with the specified parent and text.
+	 * 
+	 * @param parent
+	 *            parent
+	 * @param text
+	 *            text to display, may be <code>null</code>
+	 * @return the {@link Label}
+	 */
+	public static Label createLabel(Composite parent, String text) {
+		Label label = new Label(parent, SWT.NULL);
+		if (text != null) {
+			label.setText(text);
+		}
+		return label;
+	}
+
+	/**
+	 * Creates a simple text field with styles {@link SWT#SINGLE} and
+	 * {@link SWT#BORDER}.
+	 * 
+	 * @param parent
+	 *            parent composite
+	 * @param text
+	 *            text for the widget, may be <code>null</code>
+	 * @return the {@link Text}
+	 */
+	public static Text createSingleBorderText(Composite parent, String text) {
+		Text theText = new Text(parent, SWT.SINGLE | SWT.BORDER);
+		if (text != null) {
+			theText.setText(text);
+		}
+		GRAB_HORIZONTAL.applyTo(theText);
+		
+		return theText;
+	}
+
+	/** */
+	private WidgetHelper() {
+		// ...
+	}
+}
